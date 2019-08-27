3864644d24187a07e68c48e7359e55ee6acb5310
protozeit <Sjlooaud@gmail.com>
2019-08-27 22:31:17 +0200

    v2

--- README.md
+++
@@ -1,1 +0,0 @@
-# ensiasit.github.io
--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@ let guy_sprite1;
 let guy_sprite2;
 let guy_sprite3;
 
+let drone_audio;
+let birdnoise;
+
 let d;
 let guy;
 
@@ -11,24 +14,35 @@ let left = 0;
 let up = 0;
 let down = 0;
 let turbo = 0;
+let stop = 0;
 
 function preload() {
-  drone_sprite = loadImage("sprites/d.png");
-  guy_sprite1 = loadImage("sprites/RedHackerGuy.png");
-  guy_sprite2 = loadImage("sprites/RedHackerGuy_HandsUp.png");
-  guy_sprite3 = loadImage("sprites/RedHackerGuy_HeadDown.png");
-  logo = loadImage("sprites/logo-pixelated.png");
+  drone_sprite = loadImage("assets/sprites/d.png");
+  guy_sprite1 = loadImage("assets/sprites/RedHackerGuy.png");
+  guy_sprite2 = loadImage("assets/sprites/RedHackerGuy_HandsUp.png");
+  guy_sprite3 = loadImage("assets/sprites/RedHackerGuy_HeadDown.png");
+  logo = loadImage("assets/sprites/logo-pixelated.png");
+
+  drone_audio = loadSound("assets/audio/drone.ogg");
+  birdnoise = loadSound("assets/audio/birdnoise.ogg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   d = new Drone();
   guy = new Animation(guy_sprite1, guy_sprite2, guy_sprite3);
+  birdnoise.setVolume(0.2);
+  birdnoise.loop();
 }
-
+ 
 function draw() {
   background(255);
-  image(logo, windowWidth / 2 - logo.width / 2, 10);
+  // drone sfx
+  drone_audio.play();
+  drone_audio.setVolume(0.02 + 0.02 * (d.velocity.mag()/12))
+
+
+  image(logo, windowWidth / 2 - logo.width / 2, windowHeight / 2 - logo.height / 2);
   guy.animate(
     0.1,
     10,
@@ -37,7 +51,7 @@ function draw() {
     guy_sprite1.height * 0.15
   );
   d.update();
-  // console.log(d.velocity);
+  // console.log(stop);
   d.show();
 }
 
@@ -61,6 +75,9 @@ function keyPressed() {
   if (keyCode === SHIFT) {
     turbo = 3;
   }
+  if (keyCode === 83) { // S key
+  	stop = 1;
+  }
 }
 
 function keyReleased() {
@@ -78,5 +95,8 @@ function keyReleased() {
   }
   if (keyCode === SHIFT) {
     turbo = 0;
+  }
+  if (keyCode === 83) { // S key
+  	stop = 0;
   }
 }
