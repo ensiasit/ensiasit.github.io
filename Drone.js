a7232054b0b647b44e2a8cca10e221e7b0476cd0
protozeit <Sjlooaud@gmail.com>
2019-08-28 19:18:10 +0200

    v2

--- Drone.js
+++ Drone.js
@@ -1,13 +1,14 @@
 function Drone() {
-	this.position = createVector(windowWidth / 2, windowHeight / 2);
+	this.position = createVector(80, windowHeight - 120);
   	this.velocity = createVector(0.1, 0);
   	this.acceleration = createVector(0, 0);
+  	this.logo_side = createVector(0,0);
   	// sprite size
   	this.scale = 0.1;
   	// air resitance
   	this.c = 0.02;
   	// movement speed
-  	this.speed = 2.5;
+  	this.speed = 2;
 	
 
 	this.update = () => {
@@ -25,6 +26,35 @@ function Drone() {
 		this.acceleration.add(dragForce);
 
 		// Collision detection
+		this.wall_collision();
+		this.logo_collision();
+		this.update_logo_side();
+
+		/*
+		let away = dist(this.position.x, this.position.y, target.x, target.y);
+
+		if (away < 60) {
+			this.velocity.x = random(-80, 80);
+			this.velocity.y = random(-80, 80);
+		}
+		*/
+
+		// propagate acceleration to movement
+		this.velocity.add(this.acceleration);
+		this.position.add(this.velocity);
+
+		// brake
+		if (stop)
+			this.velocity.mult(0.85);
+		
+		// speed limit
+		this.velocity.limit(12);
+
+		this.acceleration.mult(0);
+	}
+
+	this.wall_collision = () => {	
+
 		if (this.position.x < 0) { 
 			this.position.x = 0;
 			this.velocity.x = -this.velocity.x;
@@ -50,26 +80,64 @@ function Drone() {
 			this.position.y = windowHeight - drone_sprite.height*this.scale;
 			// no bounce on the ground
 			// this.velocity.y = -this.velocity.y;
-			up = 0;
-			down = 0;
+			// up = 0;
+			// down = 0;
 			turbo = 0
 		}
+	}
 
-		// propagate acceleration to movement
-		this.velocity.add(this.acceleration);
-		this.position.add(this.velocity);
+	this.logo_collision = () => {
+		if (collideRectRect(this.position.x, this.position.y, drone_sprite.width*this.scale, drone_sprite.height*this.scale, prelogo.x, prelogo.y, prelogo.w, prelogo.h)) {
+			if (this.logo_side.y === -1) {
+				this.position.y = prelogo.y - drone_sprite.height*this.scale;
+				turbo = 0;
+				return;
+			}
+			if (this.logo_side.y === 1) {
+				this.position.y = prelogo.y + prelogo.h;
+				turbo = 0;
+				return;
+			}
+			if (this.logo_side.x === -1) {
+				this.position.x = prelogo.x - drone_sprite.width*this.scale;
+				turbo = 0;
+				return;
+			}
+			if (this.logo_side.x === 1) {
+				this.position.x = prelogo.x + prelogo.w;
+				turbo = 0;
+				return;
+			}
+		}
+	}
 
-		// brake
-		if (stop)
-			this.velocity.mult(0.85);
-		
-		// speed limit
-		this.velocity.limit(12);
+	this.update_logo_side = () => {
 
-		this.acceleration.mult(0);
+		this.logo_side.mult(0);
+		if (collideRectRect(this.position.x, this.position.y, drone_sprite.width*this.scale, drone_sprite.height*this.scale, up_rectx, up_recty, up_rectw, up_recth))
+			this.logo_side.y = -1;
+		if (collideRectRect(this.position.x, this.position.y, drone_sprite.width*this.scale, drone_sprite.height*this.scale, down_rectx, down_recty, down_rectw, down_recth))
+			this.logo_side.y = 1;
+		if (collideRectRect(this.position.x, this.position.y, drone_sprite.width*this.scale, drone_sprite.height*this.scale, left_rectx, left_recty, left_rectw, left_recth))
+			this.logo_side.x = -1;
+		if (collideRectRect(this.position.x, this.position.y, drone_sprite.width*this.scale, drone_sprite.height*this.scale, right_rectx, right_recty, right_rectw, right_recth))
+			this.logo_side.x = 1;
+
 	}
 
 	this.show = () => {
 		image(drone_sprite, this.position.x, this.position.y, drone_sprite.width*this.scale, drone_sprite.height*this.scale);
 	}
 }
+
+function collideRectRect(x, y, w, h, x2, y2, w2, h2) {
+  //2d
+  //add in a thing to detect rectMode CENTER
+  if (x + w >= x2 &&    // r1 right edge past r2 left
+      x <= x2 + w2 &&    // r1 left edge past r2 right
+      y + h >= y2 &&    // r1 top edge past r2 bottom
+      y <= y2 + h2) {    // r1 bottom edge past r2 top
+        return true;
+  }
+  return false;
+};
--- sketch.js
+++ sketch.js
@@ -5,9 +5,33 @@ let guy_sprite3;
 
 let drone_audio;
 let birdnoise;
+let music;
 
 let d;
 let guy;
+let target;
+let prelogo;
+
+let up_rectx;
+let up_recty;
+let up_rectw;
+let up_recth;
+
+let down_rectx;
+let down_recty;
+let down_rectw;
+let down_recth;
+
+let right_rectx;
+let right_recty;
+let right_rectw;
+let right_recth;
+
+let left_rectx;
+let left_recty;
+let left_rectw;
+let left_recth;
+
 
 let right = 0;
 let left = 0;
@@ -15,6 +39,10 @@ let up = 0;
 let down = 0;
 let turbo = 0;
 let stop = 0;
+
+function value_limit(val, min, max) {
+  return val < min ? min : (val > max ? max : val);
+}
 
 function preload() {
   drone_sprite = loadImage("assets/sprites/d.png");
@@ -25,38 +53,71 @@ function preload() {
 
   drone_audio = loadSound("assets/audio/drone.ogg");
   birdnoise = loadSound("assets/audio/birdnoise.ogg");
+  music = loadSound("assets/audio/town.ogg");
+
+  pixel_font = loadFont("assets/fonts/joystix monospace.ttf");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textFont(pixel_font);
+  textSize(124);
+  textStyle(BOLD);
+  textAlign(CENTER);
+
   d = new Drone();
   guy = new Animation(guy_sprite1, guy_sprite2, guy_sprite3);
-  birdnoise.setVolume(0.2);
+  
+  setup_logo();
+
+  birdnoise.setVolume(0.1);
   birdnoise.loop();
+  music.setVolume(0.2);
+  music.loop();
+  drone_audio.loop();
+  drone_audio.playMode('restart');
 }
  
 function draw() {
   background(255);
   // drone sfx
-  drone_audio.play();
-  drone_audio.setVolume(0.02 + 0.02 * (d.velocity.mag()/12))
+  drone_audio.setVolume(0.05 + 0.05 * (d.velocity.mag()/12));
+
+  // music fx
+  let speedup = 100000 / Math.pow(dist(d.position.x, d.position.y, target.x, target.y), 3);
+  speedup = value_limit(speedup, 0, 2);
+  music.rate(1 + speedup);
 
 
-  image(logo, windowWidth / 2 - logo.width / 2, windowHeight / 2 - logo.height / 2);
+  // image(logo, windowWidth / 2 - logo.width / 2, windowHeight / 2 - logo.height / 2);
+
+  // optional hitboxes here
+  noFill();
+  // rect(prelogo.x, prelogo.y, prelogo.w, prelogo.h);
+  // rect(up_rectx, up_recty, up_rectw, up_recth);
+  // rect(down_rectx, down_recty, down_rectw, down_recth);
+  // rect(left_rectx, left_recty, left_rectw, left_recth);
+  // rect(right_rectx, right_recty, right_rectw, right_recth);
+
+
+  fill(50);
+  text('EITC', windowWidth / 2, windowHeight / 2);
+  
   guy.animate(
     0.1,
-    10,
+    20,
     windowHeight - guy_sprite1.height * 0.15,
     guy_sprite1.width * 0.15,
     guy_sprite1.height * 0.15
   );
+
   d.update();
-  // console.log(stop);
   d.show();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  setup_logo();
 }
 
 function keyPressed() {
@@ -99,4 +160,34 @@ function keyReleased() {
   if (keyCode === 83) { // S key
   	stop = 0;
   }
+}
+
+function setup_logo() {
+  target = createVector(windowWidth/2 - 50, windowHeight/2 - 80);
+  prelogo = pixel_font.textBounds('EITC', windowWidth / 2, windowHeight / 2);
+  // padding
+  prelogo.x -= 5;
+  prelogo.y -= 5;
+  prelogo.w += 10;
+  prelogo.h += 10;
+
+  up_rectx = prelogo.x;
+  up_recty = 0;
+  up_rectw = prelogo.w;
+  up_recth = prelogo.y;
+
+  down_rectx = prelogo.x;
+  down_recty = prelogo.y + prelogo.h;
+  down_rectw = prelogo.w;
+  down_recth = windowHeight - (prelogo.y + prelogo.h);
+
+  right_rectx = prelogo.x + prelogo.w;
+  right_recty = prelogo.y;
+  right_rectw = windowWidth - (prelogo.x + prelogo.w);
+  right_recth = prelogo.h;
+
+  left_rectx = 0;
+  left_recty = prelogo.y;
+  left_rectw = prelogo.x;
+  left_recth = prelogo.h;
 }
